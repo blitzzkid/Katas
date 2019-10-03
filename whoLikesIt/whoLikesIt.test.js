0c4b20dddf0e56a3fc3f05570e9da4a5f39327c3
mtan014 <mtan014@e.ntu.edu.sg>
2019-10-03 10:39:12 +0800

    Added whoLikesIt Kata

---
+++ whoLikesIt/whoLikesIt.test.js
@@ -0,0 +1,36 @@
+const likes = require("./whoLikesIt");
+
+describe("People who likes this post", () => {
+  describe("If user input some names", () => {
+    it("Passes in an empty array", () => {
+      expect(likes([])).toBe("no one likes this");
+    });
+    it("Passes in an array with 1 name", () => {
+      expect(likes(["Peter"])).toBe("Peter likes this");
+    });
+    it("Passes in an array with 2 names", () => {
+      expect(likes(["Jacob", "Alex"])).toBe("Jacob and Alex like this");
+    });
+    it("Passes in an array with 3 names", () => {
+      expect(likes(["Max", "John", "Mark"])).toBe(
+        "Max, John and Mark like this"
+      );
+    });
+    it("Passes in an array with 4 names", () => {
+      expect(likes(["Alex", "Jacob", "Mark", "Max"])).toBe(
+        "Alex, Jacob and 2 others like this"
+      );
+    });
+    it("Passes in an array with 5 names", () => {
+      expect(likes(["Alex", "Jacob", "Mark", "Max", "John"])).toBe(
+        "Alex, Jacob and 3 others like this"
+      );
+    });
+  });
+
+  describe("If the user did not input anything", () => {
+    it("should return a prompt", () => {
+      expect(likes()).toBe("no one likes this");
+    });
+  });
+});
